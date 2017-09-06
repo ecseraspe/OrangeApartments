27f5fd86befea412729015a6371a231421d14a80
Oleg Glushko <oleghlushko@gmail.com>
2017-09-06 15:11:03 +0300

    Small refactor of userController

--- OrangeApartments/Controllers/UserController.cs
+++ OrangeApartments/Controllers/UserController.cs
@@ -14,6 +14,7 @@ using System.Web;
 using System.Web.Http;
 using OrangeApartments.Filters;
 using OrangeApartments.Core.Domain.DTO;
+using OrangeApartments.Helpers;
 
 namespace OrangeApartments.Controllers
 {
@@ -38,6 +39,16 @@ namespace OrangeApartments.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, user);
         }
 
+        /// <summary>
+        /// Get user profile image
+        /// 
+        /// if user has no image than default image is returned
+        /// default image location: App_Data/Img/
+        /// default image name: default-user
+        /// 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         // GET: api/User/{id}/GetUserImg
         //public User Get(int id)
         [HttpGet]
@@ -59,18 +70,28 @@ namespace OrangeApartments.Controllers
             return response;
         }
 
+        /// <summary>
+        /// Add user image
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        /// TODO: Only authorized user can perform this action.
+        [AuthFilter]
         [Route("api/user/{userId}/SaveImg")]
         [HttpPost]
         public HttpResponseMessage SaveImage(int userId)
         {
-            Dictionary<string, object> dict = new Dictionary<string, object>();
+            var accessTokenValue = Request.Headers.GetValues("Token").FirstOrDefault();
+            var currentUserId = SessionHelper.GetSession(accessTokenValue);
 
-            // check if user exists
-            if (_uof.Users.Get(userId) == null)
-            {
-                dict.Add("error", string.Format("User not found. Image was not loaded."));
-                return Request.CreateResponse(HttpStatusCode.NotFound, dict);
-            }
+            var user = _uof.Users.SingleOrDefault(u => u.UserId == currentUserId);
+            if (user == null)
+                return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Only authorized users can perform this action");
+
+            if (userId != currentUserId)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Users are allowed to change only their profile image");
+
+            Dictionary<string, object> dict = new Dictionary<string, object>();
 
             try
             {
