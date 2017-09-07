a43c9bb12cb92444e534832dc72b376979c8555c
Oleg Glushko <oleghlushko@gmail.com>
2017-09-08 02:08:14 +0300

    connection string added

--- OrangeApartments/Migrations/201709072307271_Initial.Designer.cs
+++ OrangeApartments/Migrations/201709072307271_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace OrangeApartments.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201709071855066_initial"; }
+            get { return "201709072307271_Initial"; }
         }
         
         string IMigrationMetadata.Source
